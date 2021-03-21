fa20c0870f5b3c5857839067b67e2d1f99f780a8
tr7zw <tr7zw@live.de>
2021-03-21 14:18:47 +0100

    Update to the same state as the Fabric version

--- src/main/java/dev/tr7zw/entityculling/CullTask.java
+++ src/main/java/dev/tr7zw/entityculling/CullTask.java
@@ -1,12 +1,16 @@
 package dev.tr7zw.entityculling;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import com.logisticscraft.occlusionculling.OcclusionCullingInstance;
+import com.logisticscraft.occlusionculling.util.AxisAlignedBB;
+import com.logisticscraft.occlusionculling.util.Vec3d;
+
 import dev.tr7zw.entityculling.access.Cullable;
-import dev.tr7zw.entityculling.occlusionculling.AxisAlignedBB;
-import dev.tr7zw.entityculling.occlusionculling.OcclusionCullingInstance;
+import dev.tr7zw.entityculling.access.EntityAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
@@ -21,10 +25,9 @@ public class CullTask implements Runnable {
 
 	private final OcclusionCullingInstance culling;
 	private final Minecraft client = Minecraft.getInstance();
-	private final AxisAlignedBB blockAABB = new AxisAlignedBB(0d, 0d, 0d, 1d, 1d, 1d);
 	private final int sleepDelay = 10;
 	private final Set<TileEntityType<?>> unCullable;
-	private Vector3d lastPos = new Vector3d(0, 0, 0);
+	private Vec3d lastPos = new Vec3d(0, 0, 0);
 	private long lastTime = 0;
 
 	public CullTask(OcclusionCullingInstance culling, Set<TileEntityType<?>> unCullable) {
@@ -38,21 +41,30 @@ public class CullTask implements Runnable {
 			try {
 				Thread.sleep(sleepDelay);
 
-				if (client.world != null && client.player != null && client.player.ticksExisted > 10) {
-					Vector3d camera = EntityCullingMod.instance.debug
-							? client.player.getEyePosition(client.getRenderPartialTicks())
-							: client.gameRenderer.getActiveRenderInfo().getProjectedView();
+				if (EntityCullingMod.enabled && client.world != null && client.player != null && client.player.ticksExisted > 10) {
+				    Vector3d cameraMC = EntityCullingMod.instance.debug
+                            ? client.player.getEyePosition(client.getRenderPartialTicks())
+                            : client.gameRenderer.getActiveRenderInfo().getProjectedView();
+					Vec3d camera = new Vec3d(cameraMC.x, cameraMC.y, cameraMC.z);
 					if (requestCull || !lastPos.equals(camera)) {
 						long start = System.currentTimeMillis();
 						requestCull = false;
 						lastPos = camera;
 						culling.resetCache();
 						boolean spectator = client.player.isSpectator();
-						for (int x = -3; x <= 3; x++) {
-							for (int z = -3; z <= 3; z++) {
-								Chunk chunk = client.world.getChunk(client.player.chunkCoordX + x,
-										client.player.chunkCoordZ + z);
-								for (Entry<BlockPos, TileEntity> entry : chunk.getTileEntityMap().entrySet()) {
+						for (int x = -8; x <= 8; x++) {
+							for (int z = -8; z <= 8; z++) {
+							    Chunk chunk = client.world.getChunk(client.player.chunkCoordX + x,
+                                        client.player.chunkCoordZ + z);
+								Iterator<Entry<BlockPos, TileEntity>> iterator = chunk.getTileEntityMap().entrySet().iterator();
+								Entry<BlockPos, TileEntity> entry;
+								while(iterator.hasNext()) {
+									try {
+										entry = iterator.next();
+									}catch(NullPointerException | ConcurrentModificationException ex) {
+										break; // We are not synced to the main thread, so NPE's are allowed here and way less
+										// overhead probably than trying to sync stuff up for no really good reason
+									}
 									if(unCullable.contains(entry.getValue().getType())) {
 										continue;
 									}
@@ -63,10 +75,11 @@ public class CullTask implements Runnable {
 											continue;
 										}
 										BlockPos pos = entry.getKey();
-										boolean visible = culling.isAABBVisible(
-												new Vector3d(pos.getX(), pos.getY(), pos.getZ()), blockAABB, camera,
-												false);
-										cullable.setCulled(!visible);
+										if(pos.withinDistance(cameraMC, 64)) { // 64 is the fixed max tile view distance
+    										boolean visible = culling.isAABBVisible(
+    										        new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX()+1d, +pos.getY()+1d, pos.getZ()+1d), camera);
+    										cullable.setCulled(!visible);
+										}
 									}
 								}
 
@@ -83,18 +96,20 @@ public class CullTask implements Runnable {
 							}
 							Cullable cullable = (Cullable) entity;
 							if (!cullable.isForcedVisible()) {
-								if (spectator || entity.isGlowing()) {
+								if (spectator || ((EntityAccessor)entity).isUnsafeGlowing()) {
 									cullable.setCulled(false);
 								} else {
-									net.minecraft.util.math.AxisAlignedBB boundingBox = entity.getRenderBoundingBox();
-									boolean visible = culling.isAABBVisible(
-											new Vector3d(entity.getPosX(), entity.getPosY(),
-													entity.getPosZ()),
-											new AxisAlignedBB(boundingBox.minX - 0.05, boundingBox.minY,
-													boundingBox.minZ - 0.05, boundingBox.maxX + 0.05, boundingBox.maxY,
-													boundingBox.maxZ + 0.05),
-											camera, true);
-									cullable.setCulled(!visible);
+								    if(entity.getPositionVec().isWithinDistanceOf(cameraMC, 128)) { // Max supported range currently for this mod
+								        net.minecraft.util.math.AxisAlignedBB boundingBox = entity.getRenderBoundingBox();
+    									boolean visible = culling.isAABBVisible(
+    											new AxisAlignedBB(boundingBox.minX, boundingBox.minY,
+    													boundingBox.minZ, boundingBox.maxX, boundingBox.maxY,
+    													boundingBox.maxZ),
+    											camera);
+    									cullable.setCulled(!visible);
+								    } else {
+								        cullable.setCulled(false); // If your entity view distance is larger than 128 blocks just render it
+								    }
 								}
 							}
 						}
@@ -107,5 +122,6 @@ public class CullTask implements Runnable {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("Shutting down culling task!");
 	}
 }
